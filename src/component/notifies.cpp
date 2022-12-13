76f5b49d46d4c314740ff3a0353b2a1e7d9fabbb
fed <58637860+fedddddd@users.noreply.github.com>
2022-12-14 00:33:26 +0100

    Add target entity to notify group

--- src/component/notifies.cpp
+++ src/component/notifies.cpp
@@ -14,6 +14,7 @@ namespace notifies
         struct notify_group
         {
             unsigned int owner_id;
+            unsigned int notify_target;
             unsigned int main_notify;
             std::unordered_set<unsigned int> sub_notifies;
         };
@@ -32,7 +33,7 @@ namespace notifies
                     if (i->owner_id == notify_list_owner_id &&
                         i->sub_notifies.find(string_value) != i->sub_notifies.end())
                     {
-                        vm_notify_hook.invoke<void>(inst, notify_list_owner_id, i->main_notify, top);
+                        vm_notify_hook.invoke<void>(inst, i->notify_target, i->main_notify, top);
                         i = groups.erase(i);
                     }
                     else
@@ -69,10 +70,16 @@ namespace notifies
                     throw std::runtime_error("argument 1 must be a script object");
                 }
 
-                const auto main_notify_str = args[1].as<std::string>();
+                const auto entity_target = args[1].get_raw();
+                if (entity_target.type != game::SCRIPT_OBJECT)
+                {
+                    throw std::runtime_error("argument 1 must be a script object");
+                }
+
+                const auto main_notify_str = args[2].as<std::string>();
                 const auto main_notify = game::SL_GetString(main_notify_str.data(), 0);
 
-                for (auto i = 2u; i < args.size(); i++)
+                for (auto i = 3u; i < args.size(); i++)
                 {
                     const auto& arg = args[i];
                     if (arg.is<scripting::array>())
@@ -103,7 +110,8 @@ namespace notifies
 
                 notify_groups_queue.access([&](notify_groups_t& groups)
                 {
-                    groups.emplace_back(entity.u.uintValue, main_notify, notifies);
+                    groups.emplace_back(entity.u.uintValue, entity_target.u.uintValue, 
+                        main_notify, notifies);
                 });
 
                 return {};
